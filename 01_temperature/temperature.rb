af418a94fb4fcff56538b614fa81795c52700fc3
NatTheBat <natacha.agafonov@gmail.com>
2017-10-10 15:23:50 +0200

    ajout conversion C to F

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -3,15 +3,28 @@
 
 def ftoc(far_temperature)
 
-
-  if far_temperature.is_a? Float
- far_temperature = far_temperature
-  	
-  else
+# si la valeur de la variable far_temperature est un Fixnum, il faut le transformer en Float
+  unless far_temperature.is_a? Float
+   	
   	far_temperature = far_temperature.to_f
 
   end
 
-  cel_temperature = ( far_temperature - 32.0 ) * 5 / 9
+  # opération pour convertir les Farenheit en Celsius
+    cel_temperature = ( far_temperature - 32.0 ) * 5.0 / 9.0
 
 end
+
+
+def ctof (cel_temperature)
+	
+	unless cel_temperature.is_a? Float
+
+	 	cel_temperature = cel_temperature.to_f
+
+	 end 
+
+	far_temperature = ( cel_temperature * 9.0 / 5.0 ) + 32.0
+		
+end
+
