407bc0f695d477ae337c3a1e3e670370ab04b81b
NatTheBat <natacha.agafonov@gmail.com>
2017-10-10 15:11:54 +0200

    convertit F en C

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,17 @@
 #write your code here
+
+
+def ftoc(far_temperature)
+
+
+  if far_temperature.is_a? Float
+ far_temperature = far_temperature
+  	
+  else
+  	far_temperature = far_temperature.to_f
+
+  end
+
+  cel_temperature = ( far_temperature - 32.0 ) * 5 / 9
+
+end
